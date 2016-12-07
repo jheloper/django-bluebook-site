ed1617b09bd80da24c4290e418a3f25a0ecee716
Joong Hyeon Kim <karis0222@gmail.com>
2016-12-08 00:27:31 +0900

    태그 기능 추가.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from django.views.generic.dates import ArchiveIndexView, YearArchiveView, MonthArchiveView
 from django.views.generic.dates import DayArchiveView, TodayArchiveView
-from blog.models import Post
 
-# Create your views here.
+from blog.models import Post
+from tagging.models import Tag, TaggedItem
+from tagging.views import TaggedObjectList
 
 
 class PostLV(ListView):
@@ -41,3 +42,12 @@ class PostDAV(DayArchiveView):
 class PostTAV(TodayArchiveView):
     model = Post
     date_field = 'modify_date'
+
+
+class TagTV(TemplateView):
+    template_name = 'tagging/tagging_cloud.html'
+
+
+class PostTOL(TaggedObjectList):
+    model = Post
+    template_name = 'tagging/tagging_post_list.html'
