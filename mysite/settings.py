6121ab6ec84f96861d2c33dc036bd5f846255765
Joong Hyeon Kim <karis0222@gmail.com>
2016-12-13 17:44:27 +0900

    회원가입 및 로그인, 로그아웃 기능 추가.

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,3 +137,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'www_static')
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# LOGIN_URL = '/accounts/login/'
+# LOGOUT_URL = '/accounts/logout/'
+LOGIN_REDIRECT_URL = '/'
