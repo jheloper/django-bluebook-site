51ee894d53ead3bfbc2bf46428587156ff2b3471
Joong Hyeon Kim <karis0222@gmail.com>
2016-12-15 18:43:15 +0900

    포토 앱 컨텐츠 추가, 편집, 삭제 기능 구현.

--- photo/models.py
+++ photo/models.py
@@ -1,4 +1,6 @@
 from __future__ import unicode_literals
+
+from django.contrib.auth.models import User
 from django.utils.encoding import python_2_unicode_compatible
 from django.db import models
 from django.core.urlresolvers import reverse
@@ -9,6 +11,7 @@ from photo.fields import ThumbnailImageField
 class Album(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField('One Line Description', max_length=100, blank=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['name']
@@ -26,6 +29,7 @@ class Photo(models.Model):
     image = ThumbnailImageField(upload_to='photo/%Y/%m')
     description = models.TextField('Photo Description', blank=True)
     upload_date = models.DateTimeField('Upload Date', auto_now_add=True)
+    owner = models.ForeignKey(User, null=True)
 
     class Meta:
         ordering = ['title']
